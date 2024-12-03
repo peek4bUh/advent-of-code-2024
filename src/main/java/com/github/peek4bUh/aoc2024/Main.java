a79f58afa7e44912141d89213c97c546852cb28f
peek4bUh <aandradeb99@gmail.com>
2024-12-03 22:36:21 +0100

    feat: add solution 2a

--- src/main/java/com/github/peek4bUh/aoc2024/Main.java
+++ src/main/java/com/github/peek4bUh/aoc2024/Main.java
@@ -1,6 +1,7 @@
 package com.github.peek4bUh.aoc2024;
 
 import com.github.peek4bUh.day01.Day1;
+import com.github.peek4bUh.day02.Day2;
 
 /**
  *
@@ -10,9 +11,11 @@ public class Main {
 
     public static void main(String[] args) {
         Day1 day1 = new Day1();
+        Day2 day2 = new Day2();
 
         day1.getPuzzle1().play();
         day1.getPuzzle2().play();
+        day2.getPuzzle1().play();
     }
 
 }
---
+++ src/main/java/com/github/peek4bUh/day02/Day2.java
@@ -0,0 +1,14 @@
+package com.github.peek4bUh.day02;
+
+import com.github.peek4bUh.aoc2024.BaseDay;
+
+/**
+ *
+ * @author peek4bUh
+ */
+public class Day2 extends BaseDay {
+
+    public Day2() {
+        super(new Puzzle1(), new Puzzle2());
+    }
+}
